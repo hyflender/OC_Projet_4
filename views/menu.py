d83a5192430ea40d2eece51033880e16fd90ef82
Hyflender <anthony.piaugeard@sfr.fr>
2024-05-02 10:58:53 +0200

    Update menu.py

--- views/menu.py
+++ views/menu.py
@@ -1,13 +1,21 @@
 # menu.py
 
 
-def menu_player():
-    print("Welcome to Chest Club Manager")
-    print("-----------------------------")
-    print("Please select an option:")
-    print("1. Create a new player")
-    print("2. Update a player's score")
-    print("3. View all players")
-    print("4. Exit")
-    choice = input("Enter the number of the option you want to select: ")
-    return choice
+class PlayerView:
+    def display_menu(self):
+        print("Menu to manage players")
+        print("----------------------------------------")
+        print("Please select an option:")
+        print("1. Create a new player")
+        print("2. Update a player's score")
+        print("3. View all players")
+        print("4. Exit")
+
+    def get_user_input(self, prompt):
+        return input(prompt)
+
+    def display_message(self, message):
+        print(message)
+
+    def display_player(self, player):
+        print(player.__dict__)
