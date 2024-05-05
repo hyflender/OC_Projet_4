32ee225a1bd8d6947c4f4b5b3b36cfd826b69ba6
Hyflender <anthony.piaugeard@sfr.fr>
2024-05-05 19:18:50 +0200

    Create player_view.py

---
+++ views/player_view.py
@@ -0,0 +1,16 @@
+# The menu for managing players - This class is responsible for displaying the menu for managing players.
+
+
+class PlayerView:
+    def display_player_menu(self):
+        """
+        Displays the menu for managing players.
+        """
+        print("Menu to manage players")
+        print("----------------------------------------")
+        print("Please select an option:")
+        print("1. Create a new player")
+        print("2. Update a player")
+        print("3. Update a player's score")
+        print("5. View all players")
+        print("6. Go back to main menu")
