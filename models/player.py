74521ac0e92f5c4671feb72442ad326c17bfe8c4
Hyflender <anthony.piaugeard@sfr.fr>
2024-05-05 19:45:49 +0200

    Update player.py

--- models/player.py
+++ models/player.py
@@ -2,7 +2,7 @@
 class Player:
     """Represents a chess player in a tournament."""
 
-    def __init__(self, first_name, last_name, birth_date, chess_id):
+    def __init__(self, first_name, last_name, birth_date, chess_id, score=0):
         """Initializes a new player with their first name, last name, birth date, and chess ID.
         Args:
             first_name (str): The player's first name.
@@ -15,7 +15,7 @@ class Player:
         self.last_name = last_name
         self.birth_date = birth_date
         self.chess_id = chess_id
-        self.score = 0
+        self.score = score
 
     def update_score(self, score):
         """Updates the player's score by adding the specified score to their current score.
@@ -24,3 +24,13 @@ class Player:
             score (int): The score to be added to the player's current score.
         """
         self.score += score
+
+    def to_dict(self):
+        """Convert the Player object to a dictionary."""
+        return {
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "birth_date": self.birth_date,
+            "chess_id": self.chess_id,
+            "score": self.score,
+        }
