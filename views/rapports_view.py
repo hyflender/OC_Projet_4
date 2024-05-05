7f86ebe311c5df8c807ae3cd84686eb03287460c
Hyflender <anthony.piaugeard@sfr.fr>
2024-05-05 19:45:39 +0200

    Update rapports_view.py

---
+++ views/rapports_view.py
@@ -0,0 +1,16 @@
+# The menu for create rapports - This class is responsible for displaying the menu for creating rapports.
+
+
+class RapportsView:
+    def display_rapports_menu(self):
+        """
+        Displays the menu for creating rapports.
+        """
+        print("Menu to create rapports")
+        print("----------------------------------------")
+        print("Please select an option:")
+        print("1. Create a rapport for players")
+        print("2. Create a rapport for a tournament")
+        print("3. Create a rapport for a match")
+        print("4. Go back to main menu")
+        print("----------------------------------------")
