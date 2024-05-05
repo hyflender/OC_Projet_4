398607c5cacc066c72ea40788d708934c4592afe
Hyflender <anthony.piaugeard@sfr.fr>
2024-05-05 19:17:32 +0200

    Create main_view.py

---
+++ views/main_view.py
@@ -0,0 +1,19 @@
+# The Main Menu View - This is the first view that the user sees when they run the program.
+
+
+class MainView:
+    def display_global_menu(self):
+        """
+        Displays the main menu options to the user.
+        """
+        print("Welcome to Chess Club Manager - Main Menu")
+        print("----------------------------------------")
+        print("Please select an option:")
+        print("1. Manage Players")
+        print("2. Manage Tournaments")
+        print("3. Manage Rounds")
+        print("4. Manage Matches")
+        print("5. Generate Rapports")
+        print("6. Want a beautiful GUI ?")
+        print("7. Exit")
+        print("----------------------------------------")
