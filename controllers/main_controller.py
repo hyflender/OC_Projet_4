6aedebeea58e2921c8e3b6f43e29dbaa13839ca4
Hyflender <anthony.piaugeard@sfr.fr>
2024-05-05 19:45:55 +0200

    Create main_controller.py

---
+++ controllers/main_controller.py
@@ -0,0 +1,34 @@
+from utils.chest import Library
+
+from views.main_view import MainView
+
+from controllers.player_controller import PlayerController
+from controllers.tournament_controller import TournamentController
+
+
+class GlobalController:
+    def __init__(self):
+        self.view = MainView()
+        self.player_controller = PlayerController()
+        self.tournament_controller = TournamentController()
+
+    def run(self):
+        while True:
+            self.view.display_global_menu()
+            choice = Library.get_user_input(
+                "Enter the number of the option you want to select: "
+            )
+
+            if choice == "1":
+                self.player_controller.run()
+            elif choice == "2":
+                self.tournament_controller.run()
+            elif choice == "3":
+                pass
+            elif choice == "7":
+                Library.display_message("Exiting the player management menu.")
+                break
+            else:
+                Library.display_message(
+                    "Invalid choice. Please enter a number between 1 and 4."
+                )
