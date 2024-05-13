2cddcbc51aefd91299e0eabd11d4984521973fda
Hyflender <anthony.piaugeard@sfr.fr>
2024-05-13 11:42:04 +0200

    Update player_controller.py

--- controllers/player_controller.py
+++ controllers/player_controller.py
@@ -1,7 +1,15 @@
-from models.Player import Player
+from models.player import Player
 from views.player_view import PlayerView
 
-from utils.library import log, get_user_input, clear_console
+from utils.library import (
+    log,
+    get_user_input,
+    clear_console,
+    get_valid_chess_id,
+    get_valid_date,
+    get_user_score,
+    get_user_choice,
+)
 
 
 class PlayerController:
@@ -13,8 +21,8 @@ class PlayerController:
         # Method to create a new player
         first_name, last_name, birth_date, chess_id = self.view.get_player_details()
         new_player = Player(first_name, last_name, birth_date, chess_id)
-        new_player.save_player()
-        self.players = Player.load_players()
+        self.players.append(new_player)
+        Player.save_players(self.players)
         log(
             f"Player {new_player.first_name} {new_player.last_name} created successfully."
         )
@@ -22,57 +30,62 @@ class PlayerController:
     def update_player(self):
         # Method to update informations of a player
         self.view.display_all_players(self.players)
-        chess_id = get_user_input("Enter the player's chess ID: ")
-        self.players = Player.load_players()
-        for player in self.players:
-            if player.chess_id == chess_id:
-                first_name, last_name, birth_date = self.view.get_player_details()
-                player.update_player(first_name, last_name, birth_date)
-                player.save_player()
-                log(
-                    f"Player {player.first_name} {player.last_name} updated successfully."
-                )
+        chess_id = get_valid_chess_id("Chess ID of the player to update: ", edit=True)
+        player = next((p for p in self.players if p.chess_id == chess_id), None)
+        if player:
+            log(
+                f"Player {player.first_name} {player.last_name} ({player.chess_id}) selected."
+            )
+            first_name = get_user_input(
+                "New First Name (leave blank to keep current): "
+            )
+            last_name = get_user_input("New Last Name (leave blank to keep current): ")
+            birth_date = get_valid_date(
+                "New Birth Date (leave blank to keep current): ", edit=True
+            )
+            player.update_player(first_name, last_name, birth_date)
+            Player.save_players(self.players)
+            log("Player updated successfully.")
+        else:
+            log("Player not found with the given chess ID.")
 
     def update_score_player(self):
         # Method to update the score of a player
-        self.view.display_all_players(self.players)
-        chess_id = self.view.get_user_chess_id()
 
-        self.players = Player.load_players()
-        player_found = False
-        for player in self.players:
-            if player.chess_id == chess_id:
-                new_score = self.view.get_new_score()
-                player.score = new_score
-                player.save_player()
-                log(
-                    f"Player {player.first_name} {player.last_name} updated successfully."
-                )
-                player_found = True
-                break
-        if not player_found:
-            log("No player found with the given chess ID.")
+        self.view.display_all_players(self.players)
+        chess_id = get_valid_chess_id(
+            "Chess ID of the player's score to update: ", edit=True
+        )
+        player = next((p for p in self.players if p.chess_id == chess_id), None)
+        if player:
+            log(
+                f"Player {player.first_name} {player.last_name} ({player.chess_id}) selected."
+            )
+            new_score = get_user_score("New Score (leave blank to keep current): ")
+            player.update_score(new_score)
+            Player.save_players(self.players)
+            log("Player's score updated successfully.")
+        else:
+            log("Player not found with the given chess ID.")
 
     def run(self):
         clear_console()
         while True:
+            # Display list of players
+            self.view.display_all_players(self.players)
             # Display the player menu
             self.view.display_player_menu()
             # Get the user choice
-            choice = get_user_input(
-                "Enter the number of the option you want to select: "
-            )
+            choice = get_user_choice(5)
 
-            if choice == "1":
+            if choice == 1:
                 self.create_player()
-            elif choice == "2":
+            elif choice == 2:
                 self.update_player()
-            elif choice == "3":
+            elif choice == 3:
                 self.update_score_player()
-            elif choice == "4":
+            elif choice == 4:
                 self.view.display_all_players(self.players)
-            elif choice == "5":
+            elif choice == 5:
                 log("Exiting the player management menu.")
                 break
-            else:
-                log("Invalid choice. Please enter a number between 1 and 5.")
