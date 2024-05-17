4078a19ee92f68fd647e3fb1b1cebbe14782f888
Hyflender <anthony.piaugeard@sfr.fr>
2024-05-17 09:11:38 +0200

    Update __init__.py
    easy import library

---
+++ utils/__init__.py
@@ -0,0 +1,11 @@
+# utils/__init__.py
+
+from .library import (
+    log,
+    get_user_input,
+    clear_console,
+    get_valid_chess_id,
+    get_valid_date,
+    get_user_score,
+    get_user_choice,
+)
