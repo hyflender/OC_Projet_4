7c290339bad82cdd7963da2df74c53fad09cd344
Hyflender <anthony.piaugeard@sfr.fr>
2024-05-17 10:15:00 +0200

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,8 @@
 # utils/__init__.py
 
+
 from .library import (
+    Log,
     log,
     get_user_input,
     clear_console,
@@ -8,3 +10,13 @@ from .library import (
     get_valid_date,
     get_user_choice,
 )
+
+__all__ = [
+    "Log",
+    "log",
+    "get_user_input",
+    "clear_console",
+    "get_valid_chess_id",
+    "get_valid_date",
+    "get_user_choice",
+]
