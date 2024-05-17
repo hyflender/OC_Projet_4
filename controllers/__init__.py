2749d0b207376720ec9055b5cf8c25c011c27ffd
Hyflender <anthony.piaugeard@sfr.fr>
2024-05-17 10:15:06 +0200

    Update __init__.py

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -3,3 +3,9 @@
 # Import controllers more easily
 from .player_controller import PlayerController
 from .tournament_controller import TournamentController
+
+
+__all__ = [
+    "PlayerController",
+    "TournamentController",
+]
