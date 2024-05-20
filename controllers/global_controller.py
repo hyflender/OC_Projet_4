ef0d4e1415c2c72d2af838fd04808948974e0782
Hyflender <anthony.piaugeard@sfr.fr>
2024-05-20 20:00:59 +0200

    Create global_controller.py

---
+++ controllers/global_controller.py
@@ -0,0 +1,44 @@
+from utils import Logger, get_user_choice, clear_console
+
+from controllers.player_controller import PlayerController
+from controllers.tournament_controller import TournamentController
+
+from views.main_view import MainView, ShowPlateau
+
+
+class GlobalController:
+    def __init__(self):
+        """
+        Initializes the GlobalController class, clear the console, setting up the main view and the plateau.
+        """
+        clear_console()
+        self.view = MainView()
+        self.show_plateau = ShowPlateau().afficher_plateau()
+
+    def run(self):
+        while True:
+            # Display the global menu
+            self.view.display_global_menu()
+            # Get the user choice
+            choice = get_user_choice(4)
+
+            if choice == 1:
+                try:
+                    Logger.info("Running the player controller")
+                    player_controller = PlayerController()
+                    player_controller.run()
+                except ValueError as e:
+                    Logger.critical(f"Critical error : {e}")
+            elif choice == 2:
+                try:
+                    Logger.info("Running the tournament controller")
+                    tournament_controller = TournamentController()
+                    tournament_controller.run()
+                except ValueError as e:
+                    Logger.critical(f"Critical error : {e}")
+
+            elif choice == 4:
+                print("Exiting the program. See you soon !")
+                break
+            else:
+                break
