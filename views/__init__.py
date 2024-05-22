3b72dd4f216d0d6b73f2c2d1b1d26b3b80fc1939
Hyflender <anthony.piaugeard@sfr.fr>
2024-05-22 10:16:53 +0200

    Update __init__.py

--- views/__init__.py
+++ views/__init__.py
@@ -1,11 +1,11 @@
 # views/__init__.py
 
 # Import views more easily
-from views.main_view import MainView, ShowPlateau
-from views.player_view import PlayerView
-from views.matchs_view import MatchsView
-from views.tournament_view import TournamentView
-from views.rapports_view import RapportsView
+from .main_view import MainView, ShowPlateau
+from .player_view import PlayerView
+from .matchs_view import MatchsView
+from .tournament_view import TournamentView
+from .rapports_view import RapportsView
 
 __all__ = [
     "MainView",
