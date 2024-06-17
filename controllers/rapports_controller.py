c14eab38f8861e8f2a1f4758de5e1f8edd67df41
Hyflender <anthony.piaugeard@sfr.fr>
2024-06-17 18:24:16 +0200

    Update rapports_controller.py

--- controllers/rapports_controller.py
+++ controllers/rapports_controller.py
@@ -11,27 +11,21 @@ class RapportsController:
     def run(self):
         while True:
             self.view.display_rapports_menu()
-            choice = get_user_choice(7)
+            choice = get_user_choice(5)
 
             if choice == 1:
                 # List of all players in alphabetical order
                 self.view.generate_players_report()
             elif choice == 2:
-                # List of all tournaments
+                # List of all tournaments details
                 self.view.generate_tournaments_report()
             elif choice == 3:
-                # nom et dates d'un tournoi donné
-                pass
-            elif choice == 4:
                 # List of tournament players in alphabetical order
                 self.view.generate_list_players_on_tournament()
-            elif choice == 5:
-                # List of all the matches of a tournament
-                self.view.generate_tournaments_report()
-            elif choice == 6:
+            elif choice == 4:
                 # Rapport flake8-html dans un répertoire "flake8_rapport"
                 self.view.generate_flake8_report()
-            elif choice == 7:
+            elif choice == 5:
                 print("Exiting the rapports menu.")
                 clear_console()
                 break
